8a2aa6519e84230c1982a2f8751b5ae07b43c449
Feluin <pascal.roecker.1997@gmail.com>
2022-12-05 23:28:29 +0100

    tag 6

---
+++ 2022/Day_05/part_1.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const {performance} = require("perf_hooks");
+
+const INPUT = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split("\n\n"); // change this if necessary
+
+const pStart = performance.now();
+const bottom = INPUT[0].split("\n").slice(-1)[0];
+const chestsInput = INPUT[0].split("\n").slice(0, -1);
+const indexes = Array.from(INPUT[0].split("\n").slice(-1)[0]).filter((value) => value !== " ").map(value => ({
+        stack: value,
+        value: bottom.indexOf(value)
+    })
+)
+let sta = {}
+
+indexes.forEach(index1 => sta[index1.stack] = chestsInput.map(value => Array.from(value)[index1.value]).filter(value => value !== " ").reverse())
+const moves = INPUT[1].split("\n").map(value => value.split(" ")).map(value => [value[1], value[3], value[5]])
+
+console.log(sta)
+moves.forEach(value => {
+        const count = Number(value[0])
+        for (let i = 0; i < count; i++) {
+            sta[value[2]].push(sta[value[1]].pop())
+        }
+    }
+)
+const result = Object.keys(sta).map(value => sta[value].pop()).join("");
+
+const pEnd = performance.now();
+
+console.log("<DESCRIPTION>: " + result);
+console.log(pEnd - pStart);
